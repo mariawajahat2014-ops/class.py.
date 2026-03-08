972926ff1b4f51f305fd431c4d73dab6961a7eb8
mariawajahat2014-ops <mariawajahat2014@gmail.com>
2026-03-08 13:40:59 +0500

    Add files via upload

---
+++ class.py
@@ -0,0 +1,8 @@
+math=int(input("enter marks for math"))
+english=int(input("enter marks for english"))
+science=int(input("enter marks for science"))
+hindi=int(input("enter marks for hindi"))
+sum=math+english+science+hindi
+print(sum)
+perc=(sum/400)*100
+print(perc)
